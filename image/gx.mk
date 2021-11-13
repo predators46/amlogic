a7dee7d1cdb0524aff8f3ddfaa2b444a658360f7
Predators <31931530+predators46@users.noreply.github.com>
2021-11-13 10:16:31 +0800

    Update gx.mk

--- image/gx.mk
+++ image/gx.mk
@@ -15,3 +15,14 @@ define Device/phicomm_n1
     kmod-brcmfmac wpad-basic-wolfssl cypress-firmware-43455-sdio
 endef
 TARGET_DEVICES += phicomm_n1
+
+define Device/meson-gxl-s905x_p212
+  DEVICE_VENDOR := Amlogic
+  DEVICE_MODEL := S905x
+  SOC := meson-gxl-s905x
+  UBOOT_DEVICE_NAME := p212
+  IMAGE/sdcard.img.gz := boot-common | auto-script meson-gxl-s905x-p212 | boot-script | boot-img | gzip | append-metadata
+  DEVICE_PACKAGES := \
+    kmod-brcmfmac wpad-basic-wolfssl cypress-firmware-43455-sdio
+endef
+TARGET_DEVICES += meson-gxl-s905x_p212
